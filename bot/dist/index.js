c6d76b4b9752800f015c9e7cddaaef4e1e93b39e
avazbek <avazbekdevleoper11@gmail.com>
2026-04-09 21:39:33 +0500

    save bot log token

--- bot/dist/index.js
+++ bot/dist/index.js
@@ -20,7 +20,7 @@ class BotManager {
     async startAll() {
         console.log("BotManager: Markazlarni qidirish boshlandi...");
         try {
-            console.log(prisma.center.toString());
+            console.log(prisma.center.botToken.toString());
             const centers = await prisma.center.findMany({
                 where: { botToken: { not: null, notIn: ["", "none", "token"] } }
             });
